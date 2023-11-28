8569f2c7c6fddfec99bf51df95bb0d2a6b3c514b
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2023-11-28 10:43:53 +0100

    encrypt methods

--- Program.cs
+++ Program.cs
@@ -1,15 +1,47 @@
-﻿namespace NewtonDemo20231128;
+﻿using System.Text;
+
+namespace NewtonDemo20231128;
 
 class Program
 {
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello, World!");
-        string encryptedText = Encrypt(",", 14);
+        string encryptedText = EncryptWithKeyUsingBytes("Hello world!", "hfjd746fh3b");
         Console.WriteLine(encryptedText);
     }
 
-    static string Encrypt(string text, int shift)
+    static string EncryptWithKey(string text, string key)
+    {
+        //string text = "Hello world!"
+        //string key = "hfjd746fh3b"
+
+        string encryptedText = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            char c = text[i];
+            int keyPos = i % key.Length;
+            char k = key[keyPos];
+            encryptedText += (char)(c ^ k);
+        }
+        return encryptedText;
+    }
+
+    static string EncryptWithKeyUsingBytes(string text, string key)
+    {
+        byte[] textBytes = Encoding.UTF8.GetBytes(text);
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        byte[] encryptedBytes = new byte[textBytes.Length];
+
+        for (int i = 0; i < textBytes.Length; i++)
+        {
+            encryptedBytes[i] = (byte)(textBytes[i] ^ keyBytes[i % keyBytes.Length]);
+        }
+
+        return Convert.ToBase64String(encryptedBytes);
+    }
+
+    static string EncryptWithShiffer(string text, int shift)
     {
         // [ 'H', 'e', 'l', 'l' ]
         char[] buffer = text.ToCharArray();
