744c088ef1c77bdf9e156fbb7b2deb661d6c2787
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2023-11-28 10:25:13 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace NewtonDemo20231128;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        string encryptedText = Encrypt(",", 14);
+        Console.WriteLine(encryptedText);
+    }
+
+    static string Encrypt(string text, int shift)
+    {
+        // [ 'H', 'e', 'l', 'l' ]
+        char[] buffer = text.ToCharArray();
+
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            char letter = buffer[i];
+            letter = (char)(letter + shift);
+
+            if (letter > 'z')
+            {
+                letter = (char)(letter - 26);
+            }
+            else if (letter < 'a')
+            {
+                letter = (char)(letter + 26);
+            }
+
+            buffer[i] = letter;
+        }
+
+        return new string(buffer);
+    }
+}
